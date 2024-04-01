a1c0b8cd0c6dd20893d2896880dc7218d48b7c29
mayank_jalan <mnkjln780@gmail.com>
2024-04-01 23:12:23 +0530

    Added leetcode problem of the day

---
+++ Leetcode/src/LengthofLastWord_58.java
@@ -0,0 +1,13 @@
+public class LengthofLastWord_58 {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+        int n=s.length();
+        for(int i=n-1;i>=0;i--){
+            if(s.charAt(i) == ' '){
+                return n-i-1;
+            }
+        }
+
+        return n;
+    }
+}
